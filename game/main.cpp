3a2fa533a7f147957b14ea27dda697f5f8c7f70f
matt <mmccain814@gmail.com>
2018-02-23 15:51:45 -0500

    Project Creation

---
+++ game/main.cpp
@@ -0,0 +1,19 @@
+/* 
+ * File:   main.cpp
+ * Author: Matt McCain <mmccain814@gmail.com>
+ *
+ * Created on February 23, 2018, 3:50 PM
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return 0;
+}
+
