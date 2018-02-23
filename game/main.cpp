e0029378628fa72eb8dec2d6703864ec5f7ccc4e
matt <mmccain814@gmail.com>
2018-02-23 17:43:56 -0500

    Started user choices

--- game/main.cpp
+++ game/main.cpp
@@ -6,14 +6,35 @@
  */
 
 #include <cstdlib>
+#include <iostream>
 
 using namespace std;
 
-/*
- * 
- */
+int getChoice();
+
+// Main Function
 int main(int argc, char** argv) {
 
-    return 0;
+    int choice;
+    
+    choice = getChoice();
+    
+    while (choice != 4) {
+        choice = getChoice();
+    }
+    
 }
 
+// getChoice function
+int getChoice() {
+    int choice;
+    
+    cout << "1 - plain items" << endl;
+    cout << "2 - helpful items" << endl;
+    cout << "3 - harmful items" << endl;
+    cout << "4 - quit program" << endl;
+    
+    cin >> choice;
+    return choice;
+    
+}
