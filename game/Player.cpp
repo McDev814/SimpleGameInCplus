ad2d02bc9a62caad0ceb29892078acbe9a001ca3
matt <mmccain814@gmail.com>
2018-02-24 20:26:07 -0500

    created way to save player info

--- game/Player.cpp
+++ game/Player.cpp
@@ -7,33 +7,65 @@
 
 #include <cstdlib>
 #include <iostream>
+#include <fstream>
 #include "Player.h"
 using namespace std;
 
-string name;
 int id;
+string name;
+double score;
+double allTimeScore;
 
 Player::Player() {
+}
+
+void initPlayer() {
+    
+    bool isSet = false;
+    string wantName;
+    string tempName;
+    int tempID = 0;
+    double tempScore = 0;
+    
     cout << "Enter a player name \n";
-    cout << "You can create as many players as  you like!\n";
     cout << "Each player name will have a different history.\n";
     cout << "Name: ";
-    getline(cin, name);    
-}
-
-Player::Player(string s)
-:name(s)
-{
-}
-
-Player::Player(const Player& orig) {
-}
-
-Player::~Player() {
-}
-
-void initName() {
+    getline(cin, wantName);
     
+    
+    
+    fstream fh("players.txt", ios::in);
+    fh.seekg(ios::beg);
+    while(fh >> tempID >> tempName >> tempScore) {
+        if (tempName == wantName) {
+            id = tempID;
+            name = tempName;
+            score = 0;
+            allTimeScore = tempScore;
+            printf("Great, your highest score so far is %f points!", allTimeScore);
+            isSet = true;
+            break;
+        }
+    }
+    fh.close();
+    if (!isSet) {
+        fh.open("players.txt", ios::app);
+        cout << "Great, you're a new player!\n";
+        id = tempID + 1;
+        name = wantName;
+        score = tempScore;
+        allTimeScore = tempScore;
+        fh << id << name << allTimeScore;
+    }
+    fh.close();
+}
+
+void setID(int x) {
+    id = x;
+}
+
+int getID() {
+    return id;
 }
 
 void setName(string s) {
@@ -42,4 +74,20 @@ void setName(string s) {
 
 string getName() {
     return name;
+}
+
+void setScore(double x) {
+    score = x;
+}
+
+double getScore() {
+    return score;
+}
+
+void setAllTimeScore(double x) {
+    allTimeScore = x;
+}
+
+double getAllTimeScore() {
+    return allTimeScore;
 }
