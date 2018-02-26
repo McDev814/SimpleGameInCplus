67bb4add989567c9734b5f3a54fe3b412b93ea02
matt <mmccain814@gmail.com>
2018-02-25 22:48:25 -0500

    Added another type of enemy.

---
+++ game/Monster.h
@@ -0,0 +1,27 @@
+/* 
+ * File:   Monster.h
+ * Author: Matt McCain <mmccain814@gmail.com>
+ *
+ * Created on February 25, 2018, 10:36 PM
+ */
+
+#ifndef MONSTER_H
+#define MONSTER_H
+
+#include "Enemy.h"
+#include <vector>
+#include <string>
+
+class Monster : public Enemy {
+public:
+    Monster();
+    int fight(float diff);
+    std::string speak();
+private:
+    std::string name;
+    std::vector<std::string> sayings;
+    int threatLevel;
+};
+
+#endif /* MONSTER_H */
+
