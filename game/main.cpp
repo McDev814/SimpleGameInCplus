46546c05e9cbb569c31231525558a630348a5516
matt <mmccain814@gmail.com>
2018-02-25 20:48:33 -0500

    Added a way to change difficulty.

--- game/main.cpp
+++ game/main.cpp
@@ -12,18 +12,17 @@ using namespace std;
 
 int menu();
 int getChoice();
-void displayMenu(int x);
-Player p;
+void changeDifficulty();
+Player player;
+float difficulty = 1;
 
-// Main Function
-int main(int argc, char** argv) {
+int main() {
     
-    p.initPlayer();
+    player.init();
     return menu();
     
 }
 
-// Menu Function
 int menu() {
     
     int choice;
@@ -33,13 +32,15 @@ int menu() {
         switch(choice) {
             case 0: 
                 cout << "\nchose to play game \n";
+                Arena arena(p, difficulty);
+                arena.play();
                 break;
             case 1: 
-                p.savePlayer();
-                p.initPlayer();
+                player.save();
+                player.init();
                 break;
             case 2: 
-                cout << "\nchose to change settings \n";
+                changeDifficulty();
                 break;
             case 3:
                 return 0;
@@ -51,15 +52,15 @@ int menu() {
     }
 }
 
-// getChoice function
 int getChoice() {
     char choice;
-    char options[4] = {'p','u','s','q'};
+    char options[4] = {'p','u','d','q'};
     
-    cout << "\n   Main Menu\n";
+    cout << "      Main Menu\n";
+    cout << "---------------------\n";
     cout << "p - play game" << endl;
     cout << "u - change user" << endl;
-    // cout << "s - settings" << endl;
+    cout << "d - change difficulty" << endl;
     cout << "q - quit\n" << endl;
     
     cin >> choice;
@@ -70,4 +71,40 @@ int getChoice() {
         if(choice == options[x]) { return x; }
     }
     
+}
+
+void changeDifficulty() {
+    int choice = 0;
+    int count = 0;
+    while (choice < 1 || choice > 5) {
+        if (count > 0) { cout << "Invalid choice, please press keys 1 - 4 and Enter.\n"; }
+        if (count > 4) {
+            system("clear");
+            break;
+        }
+        cout << "Choose Difficulty (1 - 4)\n";
+        cout << "1) Easy\n";
+        cout << "2) Normal (default)\n";
+        cout << "3) Hard\n";
+        cout << "4) Insane\n";
+        cout << "Enter choice (1-4): ";
+        cin >> choice;
+        if (choice > 0) {
+            switch(choice) {
+                case 1:
+                    difficulty = .5;
+                    break;
+                case 2:
+                    difficulty = 1;
+                    break;
+                case 3:
+                    difficulty = 2;
+                    break;
+                case 4:
+                    difficulty = 3;
+            }
+        }
+        count++;
+        system("clear");
+    }
 }
