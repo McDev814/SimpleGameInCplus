2adf683afd5b8f6e58cabce5337543d288fe2e8d
matt <mmccain814@gmail.com>
2018-02-25 23:19:21 -0500

    Added some healing items.

---
+++ game/Item.cpp
@@ -0,0 +1,13 @@
+/* 
+ * File:   Item.cpp
+ * Author: Matt McCain <mmccain814@gmail.com>
+ * 
+ * Created on February 25, 2018, 11:03 PM
+ */
+
+#include <cstdlib>
+#include "Item.h"
+using namespace std;
+
+Item::Item() {
+}
