f7b3b5e0dacff40680dc832a970344ee1356f09f
matt <mmccain814@gmail.com>
2018-02-25 20:55:40 -0500

    Declared member variables. I assume this needs to be done when Enemy object is created, so that child objects can define them???

---
+++ game/Enemy.cpp
@@ -0,0 +1,19 @@
+/* 
+ * File:   Enemy.cpp
+ * Author: Matt McCain <mmccain814@gmail.com>
+ * 
+ * Created on February 25, 2018, 3:07 PM
+ */
+
+#include <cstdlib>
+#include "Enemy.h"
+#include <string>
+#include <vector>
+using namespace std;
+
+string name;
+vector<string> sayings;
+int threatLevel;
+
+Enemy::Enemy() {
+}
