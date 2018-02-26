1bbd25f405581d0837ad8c69aa59ef9567863f7e
matt <mmccain814@gmail.com>
2018-02-26 04:03:13 -0500

    Created vectors of pointers.

--- game/Arena.h
+++ game/Arena.h
@@ -1,9 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 /* 
  * File:   Arena.h
  * Author: Matt McCain <mmccain814@gmail.com>
@@ -23,15 +17,16 @@ class Arena {
 public:
     Arena(Player p, float diff);
     void play();
+protected:
 private:
     float difficulty;
     Player p;
-    vector<Enemy> enemies;
+    std::vector<Enemy*> enemies;
     void generateEnemy();
-    vector<Item> items;
+    std::vector<Item*> items;
     void generateItems();
-    Enemy getEnemy();
-    Item getItem();
+    Enemy* getEnemy();
+    Item* getItem();
     int comeAcross();
     
 };
