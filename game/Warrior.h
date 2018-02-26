2d1f899e1b59a1831b8fdde0dda7eab8601fbdf0
matt <mmccain814@gmail.com>
2018-02-25 23:02:58 -0500

    Another enemy.

---
+++ game/Warrior.h
@@ -0,0 +1,27 @@
+/* 
+ * File:   Warrior.h
+ * Author: Matt McCain <mmccain814@gmail.com>
+ *
+ * Created on February 25, 2018, 10:50 PM
+ */
+
+#ifndef WARRIOR_H
+#define WARRIOR_H
+
+#include "Enemy.h"
+#include <vector>
+#include <string>
+
+class Warrior : public Enemy {
+public:
+    Warrior();
+    int fight(float diff);
+    std::string speak();
+private:
+    std::string name;
+    std::vector<std::string> sayings;
+    int threatLevel;
+};
+
+#endif /* WARRIOR_H */
+
