0a741b77d2d9b6bb3e4fa7f1bb8c8e15cff3b78f
matt <mmccain814@gmail.com>
2018-02-25 22:48:36 -0500

    Added another type of enemy.

---
+++ game/Monster.cpp
@@ -0,0 +1,35 @@
+/* 
+ * File:   Monster.cpp
+ * Author: Matt McCain <mmccain814@gmail.com>
+ * 
+ * Created on February 25, 2018, 10:36 PM
+ */
+
+#include "Monster.h"
+#include "Enemy.h"
+#include <cstdlib>
+#include <time.h>
+using namespace std;
+
+Monster::Monster()
+:name("Monster"), threatLevel(5),
+        sayings({"LOOK! Rocks are falling from the sky!",
+        "Argh, I'm going to smash your ugly face with my fists!",
+        "Everyone says I'm ugly, so I'm going to make you ugly too!",
+        "Hey, I'm just going to toss you off this cliff, let me know if you find anything down there."})
+{
+}
+
+int Monster::fight(float diff) {
+    
+    return threatLevel * diff;
+    
+}
+
+string Monster::speak() {
+    
+    srand (time(NULL));
+    int i = rand() % sayings.size() + 1;
+    return sayings.at(i);
+    
+}
