ce96c34317aa03d312413033de94a840c3d091b9
matt <mmccain814@gmail.com>
2018-02-25 20:54:21 -0500

    Error: ['string' & 'vector'] ... does not name a type Removed unnecessary includes and using namespace statement.

---
+++ game/Enemy.h
@@ -0,0 +1,24 @@
+/* 
+ * File:   Enemy.h
+ * Author: Matt McCain <mmccain814@gmail.com>
+ *
+ * Created on February 25, 2018, 3:07 PM
+ */
+
+#ifndef ENEMY_H
+#define ENEMY_H
+
+class Enemy {
+public:
+    Enemy();
+    virtual int fight(float diff)=0;
+    virtual std::string speak()=0;
+protected:
+    std::string name;
+    std::vector<std::string> sayings;
+    int threatLevel;
+
+};
+
+#endif /* ENEMY_H */
+
